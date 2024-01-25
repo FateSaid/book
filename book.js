fd25920c55436d5be5f123080715effec3c81122
Fatesaid <fatesaid56@gmail.com>
2024-01-25 21:37:20 +0100

    include button event

---
+++ book.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBookToLibrary(){
+    //do stuf here
+
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+    
+})
