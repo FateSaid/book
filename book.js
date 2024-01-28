a30cce515b51e60de9864b71122e083789f312f3
Fatesaid <fatesaid56@gmail.com>
2024-01-28 13:28:02 +0100

    add deleteInput function to clear input when user press add button

--- book.js
+++ book.js
@@ -7,6 +7,10 @@ function Book(title, author, pages, status){
     this.status = status;
 }
 
+Book.prototype.display = function(){
+    return `${this.title} is written by ${this.author}, which has ${this.pages} and status is ${this.status}`
+}
+
 function addBookToLibrary(){
     //do stuff here
     const title = document.getElementById('title').value;
@@ -15,6 +19,20 @@ function addBookToLibrary(){
     const status = document.getElementById('status').value;
     const book1 = new Book(title, author, pages, status);
     myLibrary.push(book1);
+    
+}
+
+function deleteInput(){
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+}
+
+function addtoRow(){
+    const table = document.querySelector('table');
+    const row = document.createElement('tr');
+    table.appendChild(row);
+
 }
 
 
@@ -23,4 +41,5 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
     e.preventDefault();
     addBookToLibrary();
+    deleteInput();
 })
