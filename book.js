15b6b74d1b7821e91dffd9a77eaa5f4114a75f62
Fatesaid <fatesaid56@gmail.com>
2024-01-28 12:06:55 +0100

    add function that stores user input in the object myLibrary

--- book.js
+++ book.js
@@ -8,12 +8,19 @@ function Book(title, author, pages, status){
 }
 
 function addBookToLibrary(){
-    //do stuf here
-
+    //do stuff here
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const status = document.getElementById('status').value;
+    const book1 = new Book(title, author, pages, status);
+    myLibrary.push(book1);
 }
+
+
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
     e.preventDefault();
-    
+    addBookToLibrary();
 })
